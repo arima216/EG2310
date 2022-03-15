f17d8522c4472c380137d0753d31ca9bea22b064
arima216 <88612502+arima216@users.noreply.github.com>
2022-03-15 21:15:22 +0800

    Add files via upload

---
+++ r2mover.py
@@ -0,0 +1,175 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+import math
+import cmath
+import time
+
+# constants
+rotatechange = 0.1
+speedchange = 0.05
+occ_bins = [-1, 0, 100, 101]
+stop_distance = 0.15
+turtlebot_speed=999
+front_angle = 30
+front_angles = range(-front_angle,front_angle+1,1)
+scanfile = 'lidar.txt'
+mapfile = 'map.txt'
+
+# code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+
+
+class AutoNav(Node):
+
+    def __init__(self):
+        super().__init__('auto_nav')
+        
+        # create publisher for moving TurtleBot
+        self.publisher_ = self.create_publisher(Twist,'cmd_vel',10)
+        # self.get_logger().info('Created publisher')
+        
+        # create subscription to track orientation
+        self.odom_subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10)
+        # self.get_logger().info('Created subscriber')
+        self.odom_subscription  # prevent unused variable warning
+        # initialize variables
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        
+        # create subscription to track occupancy
+        self.occ_subscription = self.create_subscription(
+            OccupancyGrid,
+            'map',
+            self.occ_callback,
+            qos_profile_sensor_data)
+        self.occ_subscription  # prevent unused variable warning
+        self.occdata = np.array([])
+        
+        # create subscription to track lidar
+        self.scan_subscription = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            qos_profile_sensor_data)
+        self.scan_subscription  # prevent unused variable warning
+        self.laser_range = np.array([])
+
+        self.rot_angle = 0
+
+
+    def odom_callback(self, msg):
+        # self.get_logger().info('In odom_callback')
+        orientation_quat =  msg.pose.pose.orientation
+        self.roll, self.pitch, self.yaw = euler_from_quaternion(orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w)
+
+
+    def occ_callback(self, msg):
+        # self.get_logger().info('In occ_callback')
+        # create numpy array
+        msgdata = np.array(msg.data)
+        # compute histogram to identify percent of bins with -1
+        # occ_counts = np.histogram(msgdata,occ_bins)
+        # calculate total number of bins
+        # total_bins = msg.info.width * msg.info.height
+        # log the info
+        # self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins))
+
+        # make msgdata go from 0 instead of -1, reshape into 2D
+        oc2 = msgdata + 1
+        # reshape to 2D array using column order
+        # self.occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width,order='F'))
+        self.occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width))
+        # print to file
+        np.savetxt(mapfile, self.occdata)
+
+
+    def scan_callback(self, msg):
+        # self.get_logger().info('In scan_callback')
+        # create numpy array
+        self.laser_range = np.array(msg.ranges)
+        # print to file
+        np.savetxt(scanfile, self.laser_range)
+        # replace 0's with nan
+        self.laser_range[self.laser_range==0] = np.nan
+
+    def compute(self):
+    	if self.laser_range[front_angles]>float(stop_distance):
+           self.rot_angle = (self.laser_range[90]-float(stop_distance))*math.pi/float(stop_distance)
+        else:
+           self.rot_angle = math.pi/2
+       	 
+    def rotatebot(self, rot_angle):
+        # self.get_logger().info('In rotatebot')
+        # create Twist object
+        twist = Twist()
+      
+        # start moving
+        self.get_logger().info('Start moving')
+        twist = Twist()
+        twist.linear.x = turtlebot_speed
+        twist.angular.z = rot_angle/5
+        # not sure if this is really necessary, but things seem to work more
+        # reliably with this
+        time.sleep(1)
+        self.publisher_.publish(twist)
+
+
+    def stopbot(self):
+        self.get_logger().info('In stopbot')
+        # publish to cmd_vel to move TurtleBot
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0
+        # time.sleep(1)
+        self.publisher_.publish(twist)
+
+
+    def mover(self):
+        while True:
+            self.compute();
+            self.rotatebot(self, self.rot_angle)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    auto_nav = AutoNav()
+    auto_nav.mover()
+
+    # create matplotlib figure
+    # plt.ion()
+    # plt.show()
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    auto_nav.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
