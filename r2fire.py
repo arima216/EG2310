880f3370b898b66bd16f49d46ef9c7ccb647a065
JuanJack06 <98009329+JuanJack06@users.noreply.github.com>
2022-03-31 20:56:03 +0800

    Update r2fire.py

--- r2fire.py
+++ r2fire.py
@@ -17,28 +17,28 @@ servo_pin = 15
 DC_pin1 = 23
 DC_pin2 = 24
 
-    def dc_start():
-    	# setup dc motors
-        GPIO.output(DC_pin1, GPIO.HIGH)
-        GPIO.output(DC_pin2, GPIO.HIGH)
+def dc_start():
+    # setup dc motors
+    GPIO.output(DC_pin1, GPIO.HIGH)
+    GPIO.output(DC_pin2, GPIO.HIGH)
             
 
-    def release():
-        p = GPIO.PWM(servo_pin, 50)
-# Set servo to 90 degrees as it's starting position 
-        p.start(7.5)
-        p.ChangeDutyCycle(2.5) #loading position 
-        time.sleep(0.1) #delay .1 second 
-        p.ChangeDutyCycle(9.5) #firing position 
-        time.sleep(1) #delay 1 second again
-        p.ChangeDutyCycle(7.5) #resting position 
-        time.sleep(1)
+def release():
+    p = GPIO.PWM(servo_pin, 50)
+    # Set servo to 90 degrees as it's starting position 
+    p.start(7.5)
+    p.ChangeDutyCycle(2.5) #loading position 
+    time.sleep(0.1) #delay .1 second 
+    p.ChangeDutyCycle(9.5) #firing position 
+    time.sleep(1) #delay 1 second again
+    p.ChangeDutyCycle(7.5) #resting position 
+    time.sleep(1)
         
-    def shoot():
-        #if correct_aim = True
-        self.dc_start()
-        for i in range (3)
-            self.release()
+def shoot():
+    #if correct_aim = True
+    dc_start()
+    for i in range(3)
+        release()
         time.sleep(10)
         
 try:
